2d32ad5402d9ff3b3a69fec76570b37533fa1db4
melque <melque_ex@yahoo.com.br>
2024-08-21 20:34:23 -0300

    update

--- SqlUts/__init__.py
+++ SqlUts/__init__.py
@@ -1,10 +1,50 @@
 import sqlalchemy as sa
-from sqlalchemy import orm
+from sqlalchemy import orm,engine_from_config
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import sqlalchemy as sa
 from sqlalchemy_utils import database_exists, create_database
 from datetime import datetime as dt
+from sqlalchemy.schema import MetaData
+
+Session = sessionmaker(twophase=True)
+
+binds = {}
+
+class DbSqlB:
+    base        = declarative_base()
+    #fast_executemany=True,
+    #pool_size=10,
+    #max_overflow=2,
+    #pool_recycle=300,
+    #pool_pre_ping=True,
+    #pool_use_lifo=True
+    def __init__(self,ConnectionString,**kwargs):
+        config_1 = {"sqlalchemy.url":ConnectionString,"sqlalchemy.echo": False}
+        self.engine                 = engine_from_config(config_1)
+        # self.base.metadata.drop_all(bind=self.engine)
+        # self.base.metadata.create_all(bind=self.engine)
+        #self.engine                 = sa.create_engine(ConnectionString) if not fast_executemany else sa.create_engine(ConnectionString,fast_executemany=True)
+        self.session                = scoped_session(sessionmaker(autocommit=False,autoflush=False,bind=self.engine))
+        self.orm_session            = orm.scoped_session(orm.sessionmaker())(bind=self.engine)
+        # self.base.metadata.bind     = self.engine
+        self.base        = declarative_base(metadata=MetaData(bind=self.engine))
+        self.base.query             = self.session.query_property()
+        #self.base.metadata     = MetaData(bind=self.engine) 
+
+    def create_all(self):
+        if not database_exists(self.engine.url):
+            create_database(self.engine.url)
+        self.base.metadata.create_all(self.engine)
+    
+    def update_engine(self,cls):
+        global Session
+        global binds
+        binds[cls] = self.engine
+        Session.configure(binds=binds)
+        Session = Session()
+        self.base.query = Session.query_property()
+
 
 class DbSqlA:
     base        = declarative_base()
